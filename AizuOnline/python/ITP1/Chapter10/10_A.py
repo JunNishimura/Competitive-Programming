e328dc7b874dd8967693f5caf35da4c754ae7d08
JunNishimura <n.junjun0303@gmail.com>
2019-03-17 12:53:21 +0900

    revise 10_A

--- AizuOnline/python/ITP1/Chapter10/10_A.py
+++ AizuOnline/python/ITP1/Chapter10/10_A.py
@@ -1,3 +1,7 @@
-import math
-x1, y1, x2, y2 = map(float, input().split())
-print(math.sqrt( (x2-x1)**2 + (y2-y1)**2 ))
+# import math
+# x1, y1, x2, y2 = map(float, input().split())
+# print(math.sqrt( (x2-x1)**2 + (y2-y1)**2 ))
+
+# revise
+a,b,c,d=map(float,input().split())
+print(abs(complex(c-a,d-b)))
