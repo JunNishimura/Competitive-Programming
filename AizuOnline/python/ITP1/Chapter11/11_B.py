a2c2e458c62b3a31da471fae027a9a62e3408021
JunNishimura <n.junjun0303@gmail.com>
2019-03-24 10:26:54 +0900

    Add 11_B.py

--- AizuOnline/python/ITP1/Chapter11/11_B.py
+++ AizuOnline/python/ITP1/Chapter11/11_B.py
@@ -1,35 +1,16 @@
 a = list(map(int, input().split()))
 s = ['402351', '152304', '310542', '215043']   
-
-def search(t, _a, _b, _c) -> (bool, list):
-    for i in range(len(s)):
-        t.append([_a[int(j)] for j in list(s[i])])
-        if t[0] == _b and t[1] == _c:
-            return True, t
-    return False, t
-
 for _ in range(int(input())):
     b, c = map(int, input().split())
     t = []
     t.append(a)
-    # print(t)
-    # l = -1
-    i = 0
+    i = 0; j = 0
     while True:
         if t[i][0] == b and t[i][1] == c:
             print(t[i][2])
             break
-        else:
-            t.append([])
-
-
-
-
-
-        k = search(t[0], b, c)
-        if k[0] == True:
-            print(k[1][2])
-            break
-        else:
-            l+=1
-            t = [t[int(j)] for j in list(s[l])]
+        if i == len(t)-1: # add if it finishes searching all path
+            for h in range(len(s)):
+                t.append([ t[j][int(k)] for k in list(s[h]) ])
+            j+=1
+        i+=1
