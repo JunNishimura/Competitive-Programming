ab2d3127e1c0004e53759809deafcc9c0aa20dbe
JunNishimura <n.junjun0303@gmail.com>
2019-09-25 12:13:36 +0900

    121 C

---
+++ atcoder/ABC/121/C.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() 
+{
+    int N, M;
+    cin >> N >> M;
+    unsigned long long money = 0;
+    vector<pair<unsigned long long, int>> v(N);
+
+    for (int i = 0; i < N; i++) cin >> v[i].first >> v[i].second;
+    sort(v.begin(), v.end());
+
+
+    for (int i = 0; i < N; i++) 
+    {
+        if ( M <= 0 ) break;
+
+        int m = min(M, v[i].second);
+        M -= m;
+        money += v[i].first * m;
+    }   
+    cout << money << endl;
+}
