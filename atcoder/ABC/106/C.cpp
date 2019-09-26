531a91d2ba22b586cd8d1532acced9d5ed9328db
JunNishimura <n.junjun0303@gmail.com>
2019-09-26 12:33:44 +0900

    add 106/C

---
+++ atcoder/ABC/106/C.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main() 
+{
+    string s;
+    long k;
+    cin >> s >> k;
+    for (int i = 0; i < k; i++) 
+    {
+        if (s[i] != '1')
+        {
+            cout << s[i] << endl;
+            return 0;
+        }
+    }
+    cout << 1 << endl;
+    return 0;
+}
